e70a229409460033dbfb5978e1df2abb650be026
masayuki038 <masayuki038@gmail.com>
2013-06-16 10:47:07 -0700

    add client mock

--- src/bootstrap_cowboy.erl
+++ src/bootstrap_cowboy.erl
@@ -8,9 +8,14 @@ start() ->
     ok = application:start(cowboy),
     Dispatch = cowboy_router:compile([
         {'_', [
-            {'_', websocket_handler, []}
+            {"/", toppage_handler, []},
+            {"/websocket", websocket_handler, []},
+            {"/static/[...]", cowboy_static, [
+                {directory, {priv_dir, erl_shib, [<<"static">>]}},
+                {mimetypes, {fun mimetypes:path_to_mimes/2,default}}
+            ]}
         ]}
     ]),
-    {ok, _} = cowboy:start_http(sample_http_handler, 100, [{port, 19860}], [{env, [{dispatch, Dispatch}]}]),
+    {ok, _} = cowboy:start_http(sample_http_handler, 100, [{port, 8080}], [{env, [{dispatch, Dispatch}]}]),
     ok.
     
