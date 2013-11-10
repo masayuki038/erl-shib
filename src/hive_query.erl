5772701c4be9c61c344bb7ed74f08cfc5fb5bb5e
masayuki038 <masayuki038@gmail.com>
2013-11-10 22:00:15 +0900

    rename tuple_to_list_recursively to tuple_to_list_deeply

--- src/hive_query.erl
+++ src/hive_query.erl
@@ -1,11 +1,11 @@
 -module(hive_query).
 
 -export([generate_id/2]).
--export([tuple_to_list_recursively/1]).
+-export([tuple_to_list_deeply/1]).
 
 -spec generate_id(binary(), _) -> [integer()]. 
 generate_id(Hql, Seed) ->
-    Md5_bin = erlang:md5(lists:concat([binary_to_list(Hql), ";", tuple_to_list_recursively(Seed)])),
+    Md5_bin = erlang:md5(lists:concat([binary_to_list(Hql), ";", tuple_to_list_deeply(Seed)])),
     Md5_list = binary_to_list(Md5_bin),
     lists:flatten(list_to_hex(Md5_list)).
 
@@ -21,11 +21,10 @@ hex(N) when N < 10 ->
 hex(N) when N >= 10, N < 16 ->
     $a + (N-10).
 		      
-tuple_to_list_recursively([]) -> [];
-tuple_to_list_recursively(T) when is_tuple(T) ->
-    tuple_to_list_recursively(tuple_to_list(T));
-tuple_to_list_recursively(T) when is_list(T) ->
-    [H|L] = T, 
-    lists:concat([tuple_to_list_recursively(H), tuple_to_list_recursively(L)]);
-tuple_to_list_recursively(T) ->
+tuple_to_list_deeply([]) -> [];
+tuple_to_list_deeply(T) when is_tuple(T) ->
+    tuple_to_list_deeply(tuple_to_list(T));
+tuple_to_list_deeply([H|L]) ->
+    lists:concat([tuple_to_list_deeply(H), tuple_to_list_deeply(L)]);
+tuple_to_list_deeply(T) ->
     T.
--- test/hive_query_tests.erl
+++ test/hive_query_tests.erl
@@ -8,5 +8,5 @@ md5_test() ->
     ?assertEqual("d13905dae94f1443a6fb542190891498", Qid).
 
 tuple_to_list_recursively1_test() ->
-    TupleToListR = hive_query:tuple_to_list_recursively({{2013, 7, 13}, {11, 44, 33}}),
+    TupleToListR = hive_query:tuple_to_list_deeply({{2013, 7, 13}, {11, 44, 33}}),
     ?assertEqual("2013713114433", TupleToListR).
